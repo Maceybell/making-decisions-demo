200ad6f3443de35a786652bf031fcfb83383e7e1
Macey Bell <maceytbell@gmail.com>
2022-09-28 15:42:32 -0600

    final battle plays out

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -4,7 +4,7 @@
     Jamie Lannister.
 */ 
 let jonSnowAttack = 25
-let jamieLannisterAttack = 35
+let jamieLannisterAttack = 55
 if(jonSnowAttack > jamieLannisterAttack){
     console.log("Jon Snow has better attack than Jamie Lannister")
 } else if (jamieLannisterAttack > jonSnowAttack){
@@ -29,3 +29,31 @@ if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense){
     jonSnowHealth-= (jamieLannisterAttack - jonSnowDefense)
     console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
 }
+
+if (jonSnowHealth + 50 >= 100){
+    jonSnowHealth = 100
+} else {
+        jonSnowHealth += 50
+    }
+console.log(`Jon Snow health after the health kit is ${jonSnowHealth}`)
+
+let coinLandsHeads = true
+
+if (coinLandsHeads){
+    console.log("The fight continues")
+} else {
+    console.log("John is allowed to run.")
+}
+
+//Jamie attacks Jon five times
+
+for (let i = 0; i < 5; i++) {
+     jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+     console.log(`Jon's health is now ${jonSnowHealth}`)
+}
+
+
+
+
+
+
